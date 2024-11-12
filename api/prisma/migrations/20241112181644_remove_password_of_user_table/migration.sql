fd4f7ab2870bfdad784b5125e606f544f999f03f
Pedro <71912007+pedro-augbs@users.noreply.github.com>
2024-11-12 15:22:21 -0300

    done

---
+++ api/prisma/migrations/20241112181644_remove_password_of_user_table/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "phone" TEXT
+);
+INSERT INTO "new_User" ("address", "email", "id", "name", "phone") SELECT "address", "email", "id", "name", "phone" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
